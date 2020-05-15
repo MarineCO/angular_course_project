e5570bc5e5f2dd2c3d287611d5602682d5e50316
Marine Colonge <marine.colonge@insidegroup.fr>
2020-05-15 12:31:20 +0200

    add connection and access between seperate element with property binding, event binding @Output() et @Input()

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -12,12 +12,22 @@ export class RecipeListComponent implements OnInit {
       'Risotto à la truffe',
       'Recette gourmande du risotto à la truffe',
       'https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+    ),
+    new Recipe(
+      'Cookie au beurre de cacahuètes et pépites de chocolat',
+      'Recette de petit plaisir sucré',
+      'https://images.unsplash.com/photo-1499636136210-6f4ee915583e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
     )
   ];
+  @Input() recipeName: string;
+  @Output() recipeClicked = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  todisplay(recipe: Recipe) {
+    this.recipeClicked.emit(recipe);
+  }
 }
